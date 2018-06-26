762470feabdf15d6d16a3991cd7deebcf48e4ba4
admin <15857356260@163.com>
2018-06-26 08:05:40 +0800

    更新
    更新

--- example.py
+++ example.py
@@ -4,6 +4,7 @@
 #@Author  : Nogo
 #@File    : example.py
 
+import REST.REST_API as CONST
 from REST.REST_API import REST_API
 
 api = REST_API()
@@ -18,7 +19,7 @@ r = api.get_kline('BIX_BTC','1min',100)
 print(r)
 
 #下单命令
-cm1 = api.create_order_cmd(1, 'BIX_BTC', 1, 2, 0.002, 1, 0.002)
-cm2 = api.create_order_cmd(2, 'BIX_BTC', 1, 2, 0.002, 1, 0.002)
+cm1 = api.create_order_cmd(1, 'BIX_BTC', CONST.SIDE_BUY, 100000, 0.002, CONST.TYPE_LIMIT_PRICE, 0.002)
+cm2 = api.create_order_cmd(2, 'BIX_BTC', CONST.SIDE_SELL, 100000, 0.002, CONST.TYPE_LIMIT_PRICE, 0.002)
 r = api.multi_sign_cmd([cm1, cm2])
 print(r)
